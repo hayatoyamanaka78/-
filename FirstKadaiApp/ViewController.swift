f1363ed0ffd86fe25481bf3791c038387e75e9df
山中敬人 <yamanakayoshito@yamanakayoshitonomacbook-puro.local>
2018-10-09 18:50:36 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 山中敬人 on 2018/10/09.
+//  Copyright © 2018年 hayato.yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
